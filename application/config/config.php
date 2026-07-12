d937155b5c41e2cd170c08072d8b38a7aa501267
Mohd sahil <sahilibrahimm@Mohds-MacBook-Pro.local>
2026-07-12 18:52:41 +0530

    Update config for ludozone.site VPS deployment

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://YOUR_DOMAIN.com/';
+$config['base_url'] = 'https://ludozone.site/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://YOUR_DOMAIN.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
